b08901e35274c0672f8c9d29abfbf7f162fa866f
coderqian <xiaoshanzhaguo@163.com>
2021-08-22 00:04:15 +0800

    案例：JavaScript图片库

---
+++ scripts/showPic.js
@@ -0,0 +1,8 @@
+function showPic(whichpic) {
+    // 路径变量
+    var source = whichpic.getAttribute("href");
+    // 获取占位符图片
+    var placeholder = document.getElementById("placeholder");
+    // 刷新img标签中src属性的值
+    placeholder.setAttribute("src", source);
+}
