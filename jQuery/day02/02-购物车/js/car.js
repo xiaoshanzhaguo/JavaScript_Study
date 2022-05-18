b2dbdb58f97b8f74927a857fad371d30202d062c
shanzhaguo <xiaoshanzhaguo@163.com>
2022-05-18 22:05:00 +0800

    feat: 购物车案例模块-删除商品模块

--- jQuery/day02/02-购物车/js/car.js
+++ jQuery/day02/02-购物车/js/car.js
@@ -9,7 +9,7 @@ $(function () {
         $(".j-checkbox, .checkall").prop("checked", $(this).prop("checked"));
     });
 
-    // 2. 如果小复选框被选中的个数等于3 就应该把全选按钮选上，否则全选按钮不选。
+    // 1.2 如果小复选框被选中的个数等于3 就应该把全选按钮选上，否则全选按钮不选。
     $(".j-checkbox").change(function () {
         // if (被选中的小的复选框的个数 === 3) {
         //     就要选中全选按钮
@@ -26,7 +26,7 @@ $(function () {
         }
     });
 
-    // 3. 增减商品数量模块 首先声明一个变量，当我们点击+号（increment），就让这个值++，然后赋值给文本框。
+    // 2. 增减商品数量模块 首先声明一个变量，当我们点击+号（increment），就让这个值++，然后赋值给文本框。
     $(".increment").click(function () {
         // 得到当前兄弟文本框的值
         var n = $(this).siblings(".itxt").val();
@@ -34,7 +34,7 @@ $(function () {
         n++;
         $(this).siblings(".itxt").val(n);
 
-        // 4. 计算小计模块 根据文本框的值 乘以 当前商品的价格 就是商品的小计
+        // 3. 计算小计模块 根据文本框的值 乘以 当前商品的价格 就是商品的小计
         // 当前商品的价格 p
         // var p = $(this).parent().parent().siblings(".p-price").html();
         // parents(".p-num") 返回指定的祖先元素
@@ -71,7 +71,7 @@ $(function () {
         getSum();
     });
 
-    // 5. 用户修改文本框的值 计算 小计模块
+    // 4. 用户修改文本框的值 计算 小计模块
     $(".itxt").change(function () {
         // 先得到文本框里面的值 乘以 当前商品的单价
         var n = $(this).val();
@@ -82,7 +82,7 @@ $(function () {
         getSum();
     });
 
-    // 6. 计算总计总额模块
+    // 5. 计算总计总额模块
     getSum(); // 第一次刷新页面时，默认调用它
 
     function getSum() {
@@ -99,4 +99,24 @@ $(function () {
 
         $(".price-sum em").text("￥" + money.toFixed(2));
     }
+
+    // 6. 删除商品模块
+    // (1) 商品后面的删除按钮
+    $(".p-action a").click(function () {
+        $(this).parents(".cart-item").remove();
+        getSum(); // 重新计算总计总额
+    });
+
+    // (2) 删除选中的商品
+    $(".remove-batch").click(function () {
+        // 删除的是小的复选框选中的商品  这里面有隐式迭代，会把所有的选中的复选框的爸爸选出来，再去移出
+        $(".j-checkbox:checked").parents(".cart-item").remove();
+        getSum();
+    });
+
+    // (3) 清空购物车 删除全部商品
+    $(".clear-all").click(function () {
+        $(".cart-item").remove(); // 这里还是利用隐式迭代，因为做的是相同的操作，隐式迭代非常方便
+        getSum();
+    });
 })
