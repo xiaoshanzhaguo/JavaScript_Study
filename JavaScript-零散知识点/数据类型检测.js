eeaa19768b53dc266f2fde9870ed916f4a920472
shanzhaguo <xiaoshanzhaguo@163.com>
2024-01-06 15:35:02 +0800

    feat: 数据类型检测

---
+++ JavaScript-零散知识点/数据类型检测.js
@@ -0,0 +1,26 @@
+/*
+   测试改变对象原型后，constructor不能判断该数据类型
+*/
+function Fn() {}
+
+// 改变原型为数组
+Fn.prototype = new Array(); 
+
+// 创建对象实例
+let f = new Fn();
+
+// 测试输出结果
+console.log(f.constructor === Fn);   // false
+console.log(f.constructor == Array); // true
+
+
+var a = Object.prototype.toString;
+
+console.log(a.call(2));  // [object Number]
+console. log(a.call(true));
+console.log(a.call('str'));
+console.log(a.call([]));
+console.log(a.call(function(){}));
+console.log(a.call({}));
+console.log(a.call(undefined));
+console.log(a.call(null));
